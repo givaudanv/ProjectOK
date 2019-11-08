00c876826c7901ad48aec665deacc081e86c97d3
Vincent <vincent.givaudan@hotmail.fr>
2019-11-08 18:13:07 +0100

    Shotcontroller + weapon controller + bounce

---
+++ Assets/Scripts/ShotController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotController : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        GetComponent<Rigidbody2D>().velocity = transform.up * speed;
+        Destroy(gameObject, lifetime);
+    }
+}
---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public GameObject shotPrefab;
+    public float chargeTime;
+
+    private float currentCharge;
+    private bool charging;
+
+    void Update()
+    {
+        TrackMouse();
+        ShotCharge();
+    }
+
+    void ShotCharge()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            charging = true;
+            currentCharge = 0f;
+        }
+
+        if (charging) currentCharge += Time.deltaTime;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            charging = false;
+            if (currentCharge >= chargeTime) Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(shotPrefab, transform.position, transform.rotation);
+    }
+
+    void TrackMouse()
+    {
+        Vector2 direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) - 90f;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
