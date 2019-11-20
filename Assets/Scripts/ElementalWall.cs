d421469079a0f4341479efcb37e0899918cabe74
Vincent <vincent.givaudan@hotmail.fr>
2019-11-20 12:17:00 +0100

    ElementalWall done

---
+++ Assets/Scripts/ElementalWall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementalWall : MonoBehaviour
+{
+    public Elements element;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<ShotController>().SetElement(element);
+    }
+}
