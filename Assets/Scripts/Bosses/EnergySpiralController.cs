a9a612672db09ee7839a0ac1b95f531620ae3d20
Michel Chaker <michel.chaker@protonmail.com>
2020-01-07 03:55:39 +0100

    replaced WaveController with EnergySpiral particle system

---
+++ Assets/Scripts/Bosses/EnergySpiralController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergySpiralController : MonoBehaviour {
+    public float waveSpeed;
+    public float killRadius;
+
+    private ParticleSystem.ShapeModule shape;
+
+    private void Start() {
+        shape = GetComponent<ParticleSystem>().shape;
+    }
+
+    void Update() {
+        if(shape.radius >= killRadius)
+            Destroy(gameObject);
+        shape.radius += waveSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player"))
+            other.GetComponent<PlayerController>()
+                 .Damage();
+    }
+}
